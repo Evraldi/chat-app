386e179a9fb219cfb2c6ba85d2cfee718bc70893
Evraldi <pekkapower9090@gmail.com>
2024-08-19 14:08:16 +0700

    some change

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,6 +1,6 @@
-function errorHandler(err, req, res, next) {
+function handleError(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send({ message: 'Internal Server Error' });
 }
 
-module.exports = errorHandler;
+module.exports = handleError;
